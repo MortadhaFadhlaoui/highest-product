51259be24a723bec6647584b8661f2d04ab6dd4e
MortadhaFadhlaoui <mortadhafadhlaoui@gmail.com>
2021-07-16 09:16:16 +0100

    first try

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const highestProduct=(list)=>{
-    const topThree = list.sort((a,b) => b-a).slice(0,3);
-    if(topThree.some(x=>x===0)) return 0;
-    else return topThree.reduce((a, b) => a * b);
-  }
+  const topThree = list.sort((a,b) => b-a).slice(0,3);
+  // return Zero if exists on top three
+  if(topThree.some(x=>x===0)) return 0;
+  else return topThree.reduce((a, b) => a * b);
+}
   
-  const input = [1, 10, 2, 6, 5, 3];
+const input = [1, 10, 2, 6, 5, 3];
   
-  console.log(highestProduct(input));
+console.log(highestProduct(input));
