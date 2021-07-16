73c32fa936628f503f9b74a54c14336f46e38598
MortadhaFadhlaoui <mortadhafadhlaoui@gmail.com>
2021-07-16 09:10:03 +0100

    first try

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # highest-product
+
+Create a function that, given a list of integers, returns the highest product between three of those numbers. For example, given the list [1, 10, 2, 6, 5, 3] the highest product would be 10 _ 6 _ 5 = 300
---
+++ main.js
@@ -0,0 +1,9 @@
+const highestProduct=(list)=>{
+    const topThree = list.sort((a,b) => b-a).slice(0,3);
+    if(topThree.some(x=>x===0)) return 0;
+    else return topThree.reduce((a, b) => a * b);
+  }
+  
+  const input = [1, 10, 2, 6, 5, 3];
+  
+  console.log(highestProduct(input));
